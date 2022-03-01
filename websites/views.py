d40eef34119275b885357c2ce009468b06928093
Kerim Sever <55418044+Kerims23@users.noreply.github.com>
2022-03-01 22:07:48 +0000

    flask updates

--- websites/views.py
+++ websites/views.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template, request
 
 views = Blueprint("views", __name__)
 
-@views.route("/")
+@views.route("/", methods=["POST", "GET"])
 def home():
-  name = "kerim"
-  return render_template("index.html", name = name)
+  if request.method=="POST":
+    name = request.form.get("name")
+    age = request.form.get("age")
+    return render_template("index.html", name=name, age=int(age))
+  return render_template("index.html")
+
+
+
+
+"""
+ask the user to enter their age
+if the user is older than 16 tell them they can drive
+otherwise, tell them how many years until they can drive
+"""
